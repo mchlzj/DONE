78d62add7d426665a7fc83f43435fc50faea1174
MZ <mchlzj@gmail.com>
2021-05-08 17:07:55 +0200

    initial commit try 2

---
+++ src/test/java/com/portfolio/done/DoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.done;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
